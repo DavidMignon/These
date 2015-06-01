e4e6607c07952d4522998030583f0644ea49b1cd
mignon <mignon@pasiphae.polytechnique.fr>
2015-06-01 18:20:12 +0200

    ...

---
+++ rapport/resultats/comparaisons/auto/structures.el
@@ -0,0 +1,26 @@
+(TeX-add-style-hook "structures"
+ (lambda ()
+    (LaTeX-add-labels
+     "temps_CPU")
+    (TeX-run-style-hooks
+     "hyperref"
+     "graphicx"
+     "multirow"
+     "microtype"
+     "lmodern"
+     ""
+     "fontenc"
+     "T1"
+     "babel"
+     "francais"
+     "inputenc"
+     "utf8"
+     "geometry"
+     "vmargin=2.5cm"
+     "hmargin=2.5cm"
+     "latex2e"
+     "art12"
+     "article"
+     "12pt"
+     "a4paper")))
+
