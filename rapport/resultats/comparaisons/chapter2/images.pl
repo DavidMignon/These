6d6e4b47b3b5e417ae9364fa122d765cfbc12b00
mignon <mignon@pasiphae.polytechnique.fr>
2015-07-01 18:24:19 +0200

    mc2e

---
+++ rapport/resultats/comparaisons/chapter2/images.pl
@@ -0,0 +1,122 @@
+# LaTeX2HTML 2008 (1.71)
+# Associate images original text with physical files.
+
+
+$key = q/^d;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="8" HEIGHT="15" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img15.png"
+ ALT="$^d$">|; 
+
+$key = q/{displaymath}|E(r_P,r_Q)|>S_{Vois}{displaymath};MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="20" HEIGHT="18" BORDER="0"
+ SRC="|."$dir".q|img3.png"
+ ALT="\begin{displaymath}
+\vert E(r_P,r_Q) \vert &gt; S_{Vois}
+\end{displaymath}">|; 
+
+$key = q/^e;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="9" HEIGHT="18" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img16.png"
+ ALT="$^e$">|; 
+
+$key = q/E_{GMEC};MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="60" HEIGHT="26" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img10.png"
+ ALT="$E_{GMEC}$">|; 
+
+$key = q/r_Q;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="19" HEIGHT="18" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img2.png"
+ ALT="$r_Q$">|; 
+
+$key = q/exp(H_i);MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="218" HEIGHT="29" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img20.png"
+ ALT="$exp(H_i)$">|; 
+
+$key = q/^f;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="8" HEIGHT="15" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img17.png"
+ ALT="$^f$">|; 
+
+$key = q/nin{5,10,20,30};MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="39" HEIGHT="27" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img5.png"
+ ALT="$n \in \{5,10,20,30\}$">|; 
+
+$key = q/E_{upper_bound};MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="57" HEIGHT="26" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img11.png"
+ ALT="$E_{upper\_bound}$">|; 
+
+$key = q/^b;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="9" HEIGHT="15" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img13.png"
+ ALT="$^b$">|; 
+
+$key = q/Print_Threshold=0;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="22" HEIGHT="14" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img7.png"
+ ALT="$Print\_Threshold=0$">|; 
+
+$key = q/f_{i}(a);MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="10" HEIGHT="18" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img18.png"
+ ALT="$f_{i}(a)$">|; 
+
+$key = q/T_ietT_{i+1};MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="65" HEIGHT="33" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img9.png"
+ ALT="$T_i et T_{i+1}$">|; 
+
+$key = q/^c;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="8" HEIGHT="18" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img14.png"
+ ALT="$^c$">|; 
+
+$key = q/S_{Vois};MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="133" HEIGHT="18" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img4.png"
+ ALT="$S_{Vois}$">|; 
+
+$key = q/0.1;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="123" HEIGHT="29" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img6.png"
+ ALT="$0.1$">|; 
+
+$key = q/^a;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="81" HEIGHT="26" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img12.png"
+ ALT="$^a$">|; 
+
+$key = q/frac{T_i}{T_{i+1}}=C;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="145" HEIGHT="15" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img8.png"
+ ALT="$ \frac{T_i}{T_{i+1}}=C $">|; 
+
+$key = q/H_i=-sum_{ainA}f_{i}(a)*log(f_{i}(a));MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="35" HEIGHT="29" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img19.png"
+ ALT="$H_i= - \sum_{a \in A} f_{i}(a) * log( f_{i}(a) ) $">|; 
+
+1;
+
---
+++ rapport/resultats/comparaisons/chapter2/internals.pl
@@ -0,0 +1,206 @@
+# LaTeX2HTML 2008 (1.71)
+# Associate internals original text with physical files.
+
+
+$key = q/tab:result_1_active_1O4C/;
+$ref_files{$key} = "$dir".q|node28.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:result_1_active_1R6J/;
+$ref_files{$key} = "$dir".q|node28.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:mutations_10_actives/;
+$ref_files{$key} = "$dir".q|node31.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:H+_H++/;
+$ref_files{$key} = "$dir".q|node36.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:mut_ident/;
+$ref_files{$key} = "$dir".q|node25.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/graph:best_ener_all_all/;
+$ref_files{$key} = "$dir".q|node25.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:superfamily_bestRE/;
+$ref_files{$key} = "$dir".q|node35.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:result_1_active_1BM2/;
+$ref_files{$key} = "$dir".q|node28.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/image:Align_Suboptimal/;
+$ref_files{$key} = "$dir".q|node34.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:result_1_active_2BYG/;
+$ref_files{$key} = "$dir".q|node28.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:etude_au_voisinnage/;
+$ref_files{$key} = "$dir".q|node34.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/graph:TRAJ_T/;
+$ref_files{$key} = "$dir".q|node25.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:ident_mc/;
+$ref_files{$key} = "$dir".q|node22.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/image:1G9O_5_GMEC_vs_MC/;
+$ref_files{$key} = "$dir".q|node34.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:protoMC/;
+$ref_files{$key} = "$dir".q|node15.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/graph:densit___en_s__quences2/;
+$ref_files{$key} = "$dir".q|node37.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/Sim_pos_byp3/;
+$ref_files{$key} = "$dir".q|node25.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/graph:Distrib_E_T/;
+$ref_files{$key} = "$dir".q|node25.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig-seqlogo-T_03/;
+$ref_files{$key} = "$dir".q|node25.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:T_et_I/;
+$ref_files{$key} = "$dir".q|node22.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:MC/;
+$ref_files{$key} = "$dir".q|node13.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:ener_mc/;
+$ref_files{$key} = "$dir".q|node21.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:result_10_actives/;
+$ref_files{$key} = "$dir".q|node30.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:result_1_active_1CKA/;
+$ref_files{$key} = "$dir".q|node28.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/graph:temps_CPU/;
+$ref_files{$key} = "$dir".q|node33.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:Traj_ident/;
+$ref_files{$key} = "$dir".q|node25.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:result_20_actives/;
+$ref_files{$key} = "$dir".q|node32.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/image:1M61_2_GMEC_vs_RE/;
+$ref_files{$key} = "$dir".q|node34.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:result_1_active_1M61/;
+$ref_files{$key} = "$dir".q|node28.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:result_5_actives/;
+$ref_files{$key} = "$dir".q|node29.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/Sim_pos_byph/;
+$ref_files{$key} = "$dir".q|node25.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:result_1_active_1A81/;
+$ref_files{$key} = "$dir".q|node28.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/Sim_pos_byp4/;
+$ref_files{$key} = "$dir".q|node25.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/image:1CKA_5_GMEC_vs_MC/;
+$ref_files{$key} = "$dir".q|node34.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:best_ener_all_all/;
+$ref_files{$key} = "$dir".q|node25.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:result_echec_30/;
+$ref_files{$key} = "$dir".q|node32.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/graph:densit___en_s__quences1/;
+$ref_files{$key} = "$dir".q|node37.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/chap:resultats_comparaisons/;
+$ref_files{$key} = "$dir".q|node1.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:protoRE/;
+$ref_files{$key} = "$dir".q|node16.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:prot__ines/;
+$ref_files{$key} = "$dir".q|node3.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/image:1G9O_3_GMEC_vs_H/;
+$ref_files{$key} = "$dir".q|node34.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:Alphabet/;
+$ref_files{$key} = "$dir".q|node24.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:result_no_active/;
+$ref_files{$key} = "$dir".q|node27.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:Entro_mc/;
+$ref_files{$key} = "$dir".q|node24.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:SF_mc/;
+$ref_files{$key} = "$dir".q|node23.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/image:1CKA_5_GMEC_vs_H/;
+$ref_files{$key} = "$dir".q|node34.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/graph:best_ener_by_algo/;
+$ref_files{$key} = "$dir".q|node25.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/image:1M61_2_GMEC_vs_H/;
+$ref_files{$key} = "$dir".q|node34.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:protoH/;
+$ref_files{$key} = "$dir".q|node12.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:voisinnage_rappel/;
+$ref_files{$key} = "$dir".q|node34.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
