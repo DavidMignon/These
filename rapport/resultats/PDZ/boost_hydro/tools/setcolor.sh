8b307659a9cb0bc21d02308b8678243c73f07696
David Mignon <david.mignon@polytechnique.edu>
2016-07-01 16:28:59 +0200

    hydro png

--- rapport/resultats/PDZ/boost_hydro/tools/setcolor.sh
+++ rapport/resultats/PDZ/boost_hydro/tools/setcolor.sh
@@ -1,11 +1,43 @@
 #!/bin/bash -xe
-# applique des couleurs html aux AA1l hydrophobes de FILE, un fichier boxshade.
-# ./setcolo.sh #010101 #fefefe myfile.html
+# applique des couleurs html aux AA1l hydrophobes.
+# ./setcolor.sh
 # David Mignon
 
+colors=("#ffffff" "#ffffff" "#ffffff" "#ffffff" "#000000" "#000000" "#000000" )
+bgcolors=("#000000" "#333333" "#666666" "#888888" "#aaaaaa" "#cccccc" "#eeeeee" )
 
-FILE=$1
-COLOR=$2
-BGCOLOR=$3
+hydro="L|A|F|T|V|M|P|I|C|G"
 
-perl -pe "s/<td>(L|A|F|T|V|M|P|I|C|G)<\/TD>/<td BGCOLOR=\"$BGCOLOR\"><FONT COLOR=\"$COLOR\">\$1<\/FONT><\/TD>/g" $FILE
+
+PATH=$PATH:/home/mignon/These/rapport/resultats/PDZ/boost_hydro/tools
+
+
+BASEFILE=sub.uniq.html
+
+WORKDIR=`pwd`
+
+
+for dir in  0 1 2 3 4 5 6
+
+do
+
+cd $WORKDIR/$dir
+
+COLOR=${colors[$dir]}
+BGCOLOR=${bgcolors[$dir]}
+
+if [ "$dir" == "0" ]
+then
+    FILE=$BASEFILE
+
+else
+    merge_align.pl $BASEFILE $last_dir/color.html > tmp.html
+    FILE=tmp.html
+fi
+
+perl -pe "s/<td>($hydro)<\/TD>/<td BGCOLOR=\"$BGCOLOR\"><FONT COLOR=\"$COLOR\">\$1<\/FONT><\/TD>/g" $FILE > color.html
+
+
+last_dir=../$dir
+
+done
