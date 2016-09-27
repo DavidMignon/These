3d3e41cb7ff45c5d3f59affddbe7eac91c4e528e
David Mignon <david.mignon@polytechnique.edu>
2016-09-27 18:34:42 +0200

    simil optGrad0 RP55

--- rapport/resultats/PDZ/tools/plot_simil_byseq.py
+++ rapport/resultats/PDZ/tools/plot_simil_byseq.py
@@ -39,6 +39,8 @@ def make_legend_arrow(legend, orig_handle,
     p = mpatches.FancyArrow(0, 0.5*height, width, 0, length_includes_head=True, head_width=0.75*height )
     return p
 
+
+
 proteus_simil   = np.loadtxt(proteus_file,usecols=[1])
 native_simil    = np.loadtxt(native_file,usecols=[1])
 pfam_simil      = np.loadtxt(pfam_file,usecols=[1])
@@ -49,11 +51,9 @@ canvas = FigureCanvas(fig)
 
 ax = fig.add_subplot(111)
 
-weights = 0.04*np.ones_like(proteus_simil)/len(proteus_simil)
-#weights = np.ones_like(proteus_simil)/len(proteus_simil)
+weights = 0.048*np.ones_like(proteus_simil)/len(proteus_simil)
+Y,X=np.histogram(proteus_simil,bins=15,weights=weights)
 
-Y,X=np.histogram(proteus_simil,bins=20,weights=weights)
-#Y,X=np.histogram(proteus_simil,bins=20,normed=True)
 
 newx=2*X[0] - X[1]
 X=np.insert(X,0,newx)
@@ -62,11 +62,9 @@ Y=np.append(Y,0)
 
 proteus_pfam,=ax.plot(X,Y,linewidth=2,marker='o',color='black')
 
-weights = 0.04*np.ones_like(rosetta_simil)/len(rosetta_simil)
-#weights = np.ones_like(rosetta_simil)/len(rosetta_simil)
-
+weights = 0.12*np.ones_like(rosetta_simil)/len(rosetta_simil)
 Y,X=np.histogram(rosetta_simil,bins=20,weights=weights)
-#Y,X=np.histogram(rosetta_simil,bins=20,normed=True)
+
 
 newx=2*X[0] - X[1]
 X=np.insert(X,0,newx)
@@ -77,11 +75,9 @@ Y=np.append(Y,0)
 rosetta_pfam,=ax.plot(X,Y,linewidth=2,marker='o',color='0.75')
 
 
-weights = 0.05*np.ones_like(pfam_simil)/len(pfam_simil)
-#print weights
+weights = 0.18*np.ones_like(pfam_simil)/len(pfam_simil)
+Y,X=np.histogram(pfam_simil,bins=40,weights=weights)
 
-Y,X=np.histogram(pfam_simil,bins=20,weights=weights)
-#Y,X=np.histogram(pfam_simil,bins=20,normed=True)
 
 newx=2*X[0] - X[1]
 X=np.insert(X,0,newx)
@@ -91,16 +87,16 @@ Y=np.append(Y,0)
 pfam_pfam,=ax.plot(X,Y,linewidth=2,linestyle='--',marker='o',color='black')
 
 
-arrow = plt.arrow(native_simil, 0.010, 0, -0.001,color='red',head_width=0.4, head_length=0.0005 )
+arrow = plt.arrow(native_simil, 0.02, 0, -0.0025,color='red',head_width=0.4, head_length=0.001 )
 
 
-ax.legend([proteus_pfam,rosetta_pfam,pfam_pfam,arrow], ['Proteus vs pfam seed','Rosetta vs pfam seed ','pfam vs pfam seed','native vs pfam seed'], handler_map={mpatches.FancyArrow : HandlerPatch(patch_func=make_legend_arrow),},loc=2,fontsize=11)
+ax.legend([proteus_pfam,rosetta_pfam,pfam_pfam,arrow], ['Proteus vs pfam RP55','Rosetta vs pfam RP55 ','pfam vs pfam RP55','native vs pfam RP55'], handler_map={mpatches.FancyArrow : HandlerPatch(patch_func=make_legend_arrow),},loc=2,fontsize=11)
 
 plt.title(protein_name)
 plt.xlabel("Similarity score", fontsize=17)
 plt.ylabel("Frequency", fontsize=17)
-ax.set_xlim(-10, 45)
-ax.set_ylim(0, 0.011)
+ax.set_xlim(-20,38)
+ax.set_ylim(0, 0.025)
 plt.grid()
 
 canvas.print_figure(output_file)
