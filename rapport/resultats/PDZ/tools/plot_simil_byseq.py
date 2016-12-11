61be8443b593e9b71fad9e4fe6df8ab5a2102a5d
David Mignon <david.mignon@polytechnique.edu>
2016-12-11 21:08:19 +0100

    simil modele B

--- rapport/resultats/PDZ/tools/plot_simil_byseq.py
+++ rapport/resultats/PDZ/tools/plot_simil_byseq.py
@@ -12,7 +12,7 @@ from matplotlib.figure import Figure
 from matplotlib.legend_handler import HandlerPatch
 import matplotlib.patches as mpatches
 
-
+ 
 try:
     protein_name  = sys.argv[1]
     proteus_file  = sys.argv[2]
@@ -20,6 +20,11 @@ try:
     pfam_file     = sys.argv[4]
     rosetta_file  = sys.argv[5]
     output_file   = sys.argv[6]
+    proteusw      = sys.argv[7]
+    pfamw         = sys.argv[8]
+    rosettaw      = sys.argv[9]
+    xlim_inf      = sys.argv[10]
+    xlim_sup      = sys.argv[11]
 except:
     print "usage: \n" + sys.argv[0] + " protein proteus_file native_file pfam_file rosetta_file graph_name.png \n"
 
@@ -29,6 +34,11 @@ except:
     print pfam_file     + "\n"
     print rosetta_file  + "\n"
     print output_file   + "\n"
+    print proteusw      + "\n"
+    print pfamw       + "\n"
+    print rosettaw    + "\n"
+    print xlim_inf   + "\n"
+    print xlim_sup   + "\n"    
     sys.exit(2)
 
 
@@ -46,13 +56,21 @@ native_simil    = np.loadtxt(native_file,usecols=[1])
 pfam_simil      = np.loadtxt(pfam_file,usecols=[1])
 rosetta_simil   = np.loadtxt(rosetta_file,usecols=[1])
 
+proteus_w = float(proteusw)
+pfam_w    = float(pfamw)
+rosetta_w = float(rosettaw)
+
+xlim_i=int(xlim_inf)
+xlim_s=int(xlim_sup)
+
+
 fig=plt.figure(figsize=(10,6),dpi=600)
 
 canvas = FigureCanvas(fig)
 
 ax = fig.add_subplot(111)
 
-weights = 50*np.ones_like(proteus_simil)/len(proteus_simil)
+weights = proteus_w*np.ones_like(proteus_simil)/len(proteus_simil)
 Y,X=np.histogram(proteus_simil,bins=20,weights=weights)
 
 
@@ -63,7 +81,7 @@ Y=np.append(Y,0)
 
 proteus_pfam,=ax.plot(X,Y,linewidth=2,marker='o',color='black')
 
-weights = 42*np.ones_like(rosetta_simil)/len(rosetta_simil)
+weights = rosetta_w*np.ones_like(rosetta_simil)/len(rosetta_simil)
 Y,X=np.histogram(rosetta_simil,bins=20,weights=weights)
 
 
@@ -76,7 +94,7 @@ Y=np.append(Y,0)
 rosetta_pfam,=ax.plot(X,Y,linewidth=4,marker='o',color='0.75')
 
 
-weights = 40*np.ones_like(pfam_simil)/len(pfam_simil)
+weights = pfam_w*np.ones_like(pfam_simil)/len(pfam_simil)
 Y,X=np.histogram(pfam_simil,bins=20,weights=weights)
 
 
@@ -85,18 +103,20 @@ X=np.insert(X,0,newx)
 Y=np.insert(Y,0,0)
 Y=np.append(Y,0)
 
-pfam_pfam,=ax.plot(X,Y,linewidth=2,linestyle='--',marker='o',color='black')
+#pfam_pfam,=ax.plot(X,Y,linewidth=2,linestyle='--',marker='o',color='black')
 
 
-arrow = plt.arrow(native_simil, 5, 0, -0.8,color='black',head_width=1, head_length=0.2 ,linewidth=3)
+arrow = plt.arrow(native_simil, 5, 0, -0.8,color='black',head_width=0.5, head_length=0.2 ,linewidth=3)
 
 
 #ax.legend([proteus_pfam,rosetta_pfam,pfam_pfam,arrow], ['Proteus ','Rosetta','Pfam','Native'], handler_map={mpatches.FancyArrow : HandlerPatch(patch_func=make_legend_arrow),},loc=2,fontsize=30)
 
-#plt.title(protein_name)
+#ax.legend([proteus_pfam,rosetta_pfam,arrow], ['Proteus ','Rosetta','Native'], handler_map={mpatches.FancyArrow : HandlerPatch(patch_func=make_legend_arrow),},loc=0,fontsize=20)
+
+plt.title(protein_name,fontsize=30)
 
 
-ax.text(-8,6,protein_name, fontsize=35)
+#ax.text(-8,6,protein_name, fontsize=35)
 
 #plt.tick_params(axis='both', which='major', labelsize=10)
 #plt.tick_params(axis='both', which='minor', labelsize=8)
@@ -105,7 +125,8 @@ plt.yticks(fontsize = 20)
 
 plt.xlabel("Similarity score", fontsize=28)
 plt.ylabel("Frequency", fontsize=28)
-ax.set_xlim(-10,50)
+
+ax.set_xlim(xlim_i,xlim_s)
 ax.set_ylim(0, 10)
 plt.grid()
 fig.tight_layout()
